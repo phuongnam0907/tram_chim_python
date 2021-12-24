bb8eac3ca4f401846ef642736cdf641f9b78f369
Le Phuong Nam <phuongnam0907@gmail.com>
2021-12-25 04:48:55 +0700

    add init function

--- sensor_485.py
+++ sensor_485.py
@@ -2,198 +2,270 @@
 
 # DEFINITION
 
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 import serial
 import time
 import json
 
-TOPIC = "/tram_chim_monitoring/dong_thap/"
+""" 
+PUBLIC VARIABLES
 
-gemho_001_temp_soil     = [0x02, 0x03,0x00, 0x00, 0x00, 0x01, 0x84, 0x39]
-gemho_001_hum_soil      = [0x02, 0x03,0x00, 0x01, 0x00, 0x01, 0xD5, 0xF9]
-gemho_001_ec            = [0x02, 0x03,0x00, 0x04, 0x00, 0x01, 0xC5, 0xF8]
+# List the variables that are commonly used in the program
+"""
 
-gemho_002_temperature   = [0x02, 0x03,0x00, 0x00, 0x00, 0x01, 0x84, 0x39]
-gemho_002_humidity      = [0x02, 0x03,0x00, 0x01, 0x00, 0x01, 0xD5, 0xF9]
-gemho_002_co2           = [0x02, 0x03,0x00, 0x04, 0x00, 0x01, 0xC5, 0xF8]
+MQTT_HOST = ""
+MQTT_PORT = ""
+MQTT_USERNAME = ""
+MQTT_PASSWORD = ""
+MQTT_TOPIC = ""
+SERIAL_PORT_NBIOT = ""
+BAUD_RATE_NBIOT = 9600
+SERIAL_PORT_SENSOR = ""
+BAUD_RATE_SENSOR = 9600
+LIST_SENSORS = []
 
-gemho_003_temperature   = [0x03, 0x03, 0x00, 0x00, 0x00, 0x01, 0x85, 0xE8]
-gemho_003_humidity      = [0x03, 0x03, 0x00, 0x01, 0x00, 0x01, 0xD4, 0x28]
-
-gemho_004_pm25          = [0x02, 0x03,0x00, 0x00, 0x00, 0x01, 0x84, 0x39]
-gemho_004_pm10          = [0x02, 0x03,0x00, 0x01, 0x00, 0x01, 0xD5, 0xF9]
-
-SERIAL_PORT_NBIOT = "/dev/ttyS0"
-SERIAL_PORT_SENSOR = "/dev/ttyUSB0"
-POWER_KEY = 4
-
+# Hardcode variables
 rec_buff = ''
+POWER_KEY = 4
 data_payload = {
-  "project_id": "projectid123456",
-  "project_name": "projectnameABC",
-  "station_id": "stationid123456",
-  "station_name": "stationnamABC",
-  "longitude": 106.660172,
-  "latitude": 10.762622,
-  "volt_battery": 12.2,
-  "volt_solar": 5.3,
-  "gemho_001_temp_soil": 30.1,
-  "gemho_001_hum_soil": 25.1,
-  "gemho_001_ec": 20,
-  "gemho_002_temperature": 30.2,
-  "gemho_002_humidity": 25.2,
-  "gemho_002_co2": 21,
-  "gemho_003_temperature": 30.3,
-  "gemho_003_humidity": 25.3,
-  "gemho_004_pm25": 22,
-  "gemho_004_pm10": 23
+    "project_id": "",
+    "project_name": "",
+    "station_id": "",
+    "station_name": "",
+    "longitude": 106.660172,
+    "latitude": 10.762622,
+    "volt_battery": 12.2,
+    "volt_solar": 5.3,
+    "data_sensor": {}
 }
 
-# FUNCTION
 
-def readSerial():
+""" 
+SUB-FUNCTIONS
+
+# List the functions that are commonly used in the program
+"""
+
+
+def init_config_file():
+    # get sensor config from json file
+    global LIST_SENSORS
+    f = open(".config/sensor.json")
+    LIST_SENSORS = json.load(f)
+    f.close()
+
+    # Open file .config/config.json
+    f = open(".config/config.json")
+    temp_json = json.load(f)
+
+    # get mqtt config from json file
+    global MQTT_HOST, MQTT_PORT, MQTT_USERNAME, MQTT_PASSWORD, MQTT_TOPIC
+    MQTT_HOST = temp_json['config_mqtt']['MQTT_HOST']
+    MQTT_PORT = temp_json['config_mqtt']['MQTT_PORT']
+    MQTT_USERNAME = temp_json['config_mqtt']['MQTT_USERNAME']
+    MQTT_PASSWORD = temp_json['config_mqtt']['MQTT_PASSWORD']
+    MQTT_TOPIC = temp_json['config_mqtt']['MQTT_TOPIC']
+
+    # get serial config from json file
+    global SERIAL_PORT_NBIOT, SERIAL_PORT_SENSOR, BAUD_RATE_NBIOT, BAUD_RATE_SENSOR
+    SERIAL_PORT_NBIOT = temp_json['config_serial']['SERIAL_PORT_NBIOT']
+    SERIAL_PORT_SENSOR = temp_json['config_serial']['SERIAL_PORT_SENSOR']
+    BAUD_RATE_NBIOT = temp_json['config_serial']['BAUD_RATE_NBIOT']
+    BAUD_RATE_SENSOR = temp_json['config_serial']['BAUD_RATE_SENSOR']
+
+    # get project status from json file
+    global data_payload
+    data_payload['project_id'] = temp_json['config_payload']['project_id']
+    data_payload['project_name'] = temp_json['config_payload']['project_name']
+    data_payload['station_id'] = temp_json['config_payload']['station_id']
+    data_payload['station_name'] = temp_json['config_payload']['station_name']
+
+    # Close file .config/config.json
+    f.close()
+
+
+def parse_name_sensor():
+    global data_payload
+    temp_object = {}
+    for item in LIST_SENSORS:
+        temp_object.update({item['sensor_name']: -1.0})
+    data_payload['data_sensor'] = temp_object
+
+
+def current_milli_time():
+    return round(time.time() * 1000)
+
+
+def read_serial():
+    return current_milli_time()
     bytesToRead = serial_sensor.inWaiting()
     if (bytesToRead > 0):
         out = serial_sensor.read(bytesToRead)
-        print("Received:", out)
         data_array = [b for b in out]
-        #print(len(data_array))
-        #print(data_array)
         if len(data_array) == 7:
-            value = data_array[3]*256 + data_array[4]
-            print("Decode value is:",  value)
-
-
-def powerOn(powerKey):
-    print("SIM7070X is starting:")
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setwarnings(False)
-    GPIO.setup(powerKey, GPIO.OUT)
-    time.sleep(0.1)
-    GPIO.output(powerKey, GPIO.HIGH)
-    time.sleep(2)
-    GPIO.output(powerKey, GPIO.LOW)
-    time.sleep(5)
-    serial_nbiot.flushInput()
-    print("SIM7070X is ready")
-
-
-def powerDown(powerKey):
-    print('SIM7070X is loging off:')
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setwarnings(False)
-    GPIO.setup(powerKey, GPIO.OUT)
-    GPIO.output(powerKey, GPIO.HIGH)
-    time.sleep(2)
-    GPIO.output(powerKey, GPIO.LOW)
-    time.sleep(5)
-    print('Good bye')
-
-
-def sendAt(command, back, timeout):
-    rec_buff = ""
-    serial_nbiot.write((command+'\r\n').encode())
-    time.sleep(timeout)
-    if serial_nbiot.inWaiting():
-        time.sleep(0.1)
-        rec_buff = serial_nbiot.read(serial_nbiot.inWaiting())
-    if rec_buff != "":
-        if back not in rec_buff.decode():
-            print(command + ' back:\t' + rec_buff.decode())
+            return data_array[3] * 256 + data_array[4]
+        else:
             return 0
-        else:
-            print(rec_buff.decode())
-            return 1
-    else:
-        print(command + ' no responce')
 
 
-def checkStart():
-    while True:
-        # simcom module uart may be fool,so it is better to send much times when it starts.
-        serial_nbiot.write( 'AT\r\n'.encode() )
-        time.sleep(1)
-        serial_nbiot.write( 'AT\r\n'.encode() )
-        serial_nbiot.sleep(1)
-        serial_nbiot.write( 'AT\r\n'.encode() )
-        time.sleep(1)
-        if serial_nbiot.inWaiting():
-            time.sleep(0.01)
-            recBuff = serial_nbiot.read(serial_nbiot.inWaiting())
-            print('SOM7080X is ready\r\n')
-            print( 'try to start\r\n' + recBuff.decode() )
-            if 'OK' in recBuff.decode():
-                recBuff = ''
-                break
-        else:
-            powerOn(POWER_KEY)
+# def getGpsPosition():
+#     rec_null = True
+#     answer = 0
+#     print('Start GPS session...')
+#     rec_buff = ''
+#     time.sleep(5)
+#     sendAt('AT+CGNSPWR=1', 'OK', 0.1)
+#     while rec_null:
+#         answer = sendAt('AT+CGNSINF', '+CGNSINF: ', 1)
+#         if 1 == answer:
+#             answer = 0
+#             if ',,,,,,' in rec_buff:
+#                 print('GPS is not ready')
+#                 rec_null = False
+#                 time.sleep(1)
+#         else:
+#             print('error %d' % answer)
+#             rec_buff = ''
+#             sendAt('AT+CGNSPWR=0', 'OK', 1)
+#             return False
+#         time.sleep(1.5)
 
 
-def sendData():
-    try:
-        checkStart()
-        print('wait for signal')
-        time.sleep(5)
-        # sendAt('AT+SMCONF?', 'OK', 5)
-        sendAt('AT+CSQ', 'OK', 1)
-        sendAt('AT+CPSI?', 'OK', 1)
-        sendAt('AT+CNMP=38', 'OK', 5)
-        sendAt('AT+CGREG?', '+CGREG: 0,1', 0.5)
-        sendAt('AT+CNACT=0,1', 'OK', 1)
-        sendAt('AT+CACID=0', 'OK', 1)
-        sendAt('AT+CNACT?', 'OK', 1)
-        sendAt('AT+SMCONF=\"CLIENTID\",id123331', 'OK', 1)
-        sendAt('AT+SMCONF=\"USERNAME\",mqttbroker', 'OK', 5)
-        sendAt('AT+SMCONF=\"PASSWORD\",Mqtt!@#456', 'OK', 5)
-        sendAt('AT+SMCONF=\"URL\",mqttserver.tk,1883', 'OK', 1)
-        sendAt('AT+SMCONF=\"KEEPTIME\",60', 'OK', 1)
-        sendAt('AT+SMCONF?', 'OK', 5)
-        sendAt('AT+SMCONN', 'OK', 5)
-        sendAt('AT+SMSUB=\"waveshare_pub\",1', 'OK', 1)
-        sendAt('AT+SMPUB=\"waveshare_sub\",17,1,0', 'OK', 1)
-        serial_nbiot.write(json.dumps(data_payload).encode())
-        time.sleep(10);
-        print('send message successfully!')
-        sendAt('AT+SMDISC', 'OK', 1)
-        sendAt('AT+CNACT=0,0', 'OK', 1)
-        powerDown(POWER_KEY)
-    except:
-        if serial_nbiot != None:
-            serial_nbiot.close()
-        powerDown(POWER_KEY)
-        GPIO.cleanup()
+def get_sensors_value():
+    for item in LIST_SENSORS:
+        try:
+            # serial_sensor.write(serial.to_bytes(item['sensor_byte']))
+            time.sleep(1)
+            data_payload['data_sensor'][item['sensor_name']] = round(read_serial() * item['sensor_unit'], 0 if isinstance(item['sensor_unit'], int) else 1 )
+        except:
+            # print("get_sensors_value: get FAILED")
+            pass
+
+# def powerOn(powerKey):
+#     print("SIM7070X is starting:")
+#     GPIO.setmode(GPIO.BCM)
+#     GPIO.setwarnings(False)
+#     GPIO.setup(powerKey, GPIO.OUT)
+#     time.sleep(0.1)
+#     GPIO.output(powerKey, GPIO.HIGH)
+#     time.sleep(2)
+#     GPIO.output(powerKey, GPIO.LOW)
+#     time.sleep(5)
+#     serial_nbiot.flushInput()
+#     print("SIM7070X is ready")
+#
+#
+# def powerDown(powerKey):
+#     print('SIM7070X is loging off:')
+#     GPIO.setmode(GPIO.BCM)
+#     GPIO.setwarnings(False)
+#     GPIO.setup(powerKey, GPIO.OUT)
+#     GPIO.output(powerKey, GPIO.HIGH)
+#     time.sleep(1)
+#     GPIO.output(powerKey, GPIO.LOW)
+#     time.sleep(3)
+#     print('Good bye')
+#
+#
+# def sendAt(command, back, timeout):
+#     rec_buff = ""
+#     serial_nbiot.write((command + '\r\n').encode())
+#     time.sleep(timeout)
+#     if serial_nbiot.inWaiting():
+#         time.sleep(0.1)
+#         rec_buff = serial_nbiot.read(serial_nbiot.inWaiting())
+#     if rec_buff != "":
+#         if back not in rec_buff.decode():
+#             print(command + ' back:\t' + rec_buff.decode())
+#             return 0
+#         else:
+#             print(rec_buff.decode())
+#             return 1
+#     else:
+#         print(command + ' no responce')
+#
+#
+# def checkStart():
+#     while True:
+#         # simcom module uart may be fool,so it is better to send much times when it starts.
+#         serial_nbiot.write('AT\r\n'.encode())
+#         time.sleep(1)
+#         serial_nbiot.write('AT\r\n'.encode())
+#         time.sleep(1)
+#         serial_nbiot.write('AT\r\n'.encode())
+#         time.sleep(1)
+#         if serial_nbiot.inWaiting():
+#             time.sleep(0.01)
+#             recBuff = serial_nbiot.read(serial_nbiot.inWaiting())
+#             print('SOM7080X is ready\r\n')
+#             print('try to start\r\n' + recBuff.decode())
+#             if 'OK' in recBuff.decode():
+#                 recBuff = ''
+#                 break
+#         else:
+#             powerOn(POWER_KEY)
+#
+#
+# def send_data():
+#     try:
+#         powerDown(POWER_KEY)
+#         checkStart()
+#         print('wait for signal')
+#         time.sleep(10)
+#         sendAt('AT+CSQ', 'OK', 1)
+#         sendAt('AT+CNMP=38', 'OK', 1)
+#         sendAt('AT+CGREG?', '+CGREG: 0,1', 0.5)
+#         sendAt('AT+CNACT=0,1', 'OK', 1)
+#         sendAt('AT+CACID=0', 'OK', 1)
+#         # getGpsPosition()
+#         sendAt('AT+SMCONF=\"CLIENTID\",id_' + str(current_milli_time()), 'OK', 0.25)
+#         sendAt('AT+SMCONF=\"USERNAME\",' + MQTT_USERNAME, 'OK', 0.25)
+#         sendAt('AT+SMCONF=\"PASSWORD\",' + MQTT_PASSWORD, 'OK', 0.25)
+#         sendAt('AT+SMCONF=\"URL\",' + MQTT_PORT + ',' + MQTT_PORT, 'OK', 0.25)
+#         sendAt('AT+SMCONF=\"KEEPTIME\",60', 'OK', 0.25)
+#         sendAt('AT+SMCONN', 'OK', 5)
+#         sendAt('AT+SMPUB=\"' + MQTT_TOPIC + '\",' + str(len(json.dumps(data_payload))) + ',0,0', 'OK', 1)
+#         serial_nbiot.write(json.dumps(data_payload).encode())
+#         time.sleep(10)
+#         print('send message successfully!')
+#         sendAt('AT+SMDISC', 'OK', 1)
+#         sendAt('AT+CNACT=0,0', 'OK', 1)
+#         powerDown(POWER_KEY)
+#     except:
+#         print('send_data: Exception')
+#         powerDown(POWER_KEY)
+#         GPIO.cleanup()
 
 
 if __name__ == '__main__':
     print("************* GEMHO Sensor 485 *************")
 
-    serial_nbiot = serial.Serial(port=SERIAL_PORT_NBIOT, baudrate=9600)
-    serial_nbiot.flushInput()
-    serial_sensor = serial.Serial(port=SERIAL_PORT_SENSOR, baudrate=9600)
+    init_config_file()
+    parse_name_sensor()
+
+    try:
+        serial_nbiot = serial.Serial(port=SERIAL_PORT_NBIOT, baudrate=BAUD_RATE_NBIOT)
+        serial_nbiot.flushInput()
+        serial_sensor = serial.Serial(port=SERIAL_PORT_SENSOR, baudrate=BAUD_RATE_SENSOR)
+    except:
+        print("Cannot open Serial Port")
+        pass
 
     while True:
-        serial_sensor.write(serial.to_bytes(gemho_002_temperature))
-        time.sleep(1)
-        readSerial()
+        get_sensors_value()
+        print(data_payload)
+        # send_data()
+        break
+    # serial_sensor.write(serial.to_bytes(gemho_002_temperature))
+    # time.sleep(1)
+    # readSerial()
 
-        serial_sensor.write(serial.to_bytes(gemho_002_humidity))
-        time.sleep(1)
-        readSerial()
+    # serial_sensor.write(serial.to_bytes(gemho_002_humidity))
+    # time.sleep(1)
+    # readSerial()
 
-        serial_sensor.write(serial.to_bytes(gemho_002_co2))
-        time.sleep(1)
-        readSerial()
-
-        sendData()
-        time.sleep(5)
-
-    # import paho.mqtt.client as mqtt
-    #
-    # client = mqtt.Client()
-    # client.username_pw_set("mqttbroker", "Mqtt!@#456")
-    # client.connect("mqttserver.tk", 1883)  # connect to broker
-    # client.loop_start()  # start the loop
-    #
-    # client.publish(TOPIC, json.dumps(data_payload))
-    #
-    # client.loop_stop()
+    # serial_sensor.write(serial.to_bytes(gemho_002_co2))
+    # time.sleep(1)
+    # readSerial()
+    # sendData()
+    # time.sleep(10)
