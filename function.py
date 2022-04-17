4c93af1f977b844c734d7129f54c11b50cfd9d66
Phuong Nam <38576110+phuongnam0907@users.noreply.github.com>
2022-04-17 19:17:25 +0700

    Update function.py

--- function.py
+++ function.py
@@ -2,6 +2,7 @@ import constant
 import json
 import time
 import requests
+import serial
 
 
 #####################################################
@@ -61,9 +62,9 @@ def update_data_from_url(data):
     return data
 
 
-def read_sensor_data(serial, data):
-    if serial.isOpen():
-        return write_serial_data(serial, data)
+def read_sensor_data(ser, data):
+    if ser.isOpen():
+        return write_serial_data(ser, data)
     else:
         print("ERROR: Cannot open serial port")
         return -1
@@ -73,23 +74,24 @@ def publish_data_to_mqtt_server(device_client, data):
     device_client.publish(constant.MQTT_TOPIC, json.dumps(data), 0, True)
 
 
-def write_serial_data(serial, data):
+def write_serial_data(ser, data):
     try:
-        serial.write(serial.to_bytes(data))
+        ser.write(serial.to_bytes(data))
         time.sleep(1)
-        return read_serial_data(serial)
+        return read_serial_data(ser)
     except:
         print("ERROR: Failed to write data")
         return -1
 
 
-def read_serial_data(serial):
-    bytesToRead = serial.inWaiting()
+def read_serial_data(ser):
+    bytesToRead = ser.inWaiting()
     if bytesToRead > 0:
-        out = serial.read(bytesToRead)
+        out = ser.read(bytesToRead)
         data_array = [b for b in out]
         if len(data_array) == 7:
             value = data_array[3] * 256 + data_array[4]
             return value
         else:
-            return 0
+            return -1
+    return -1
