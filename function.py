4b7afe02f9f9d87fa9f0caab8c0b1b645180bcf1
Nam Le <nam.lp@becamex.com.vn>
2022-05-13 20:26:17 +0700

    init air

--- function.py
+++ function.py
@@ -7,19 +7,13 @@ import serial
 #####################################################
 # Class sensor
 class Sensor:
-    def __init__(self, name, key, data, measure_unit, calibrate_factor=None):
-        self.name = name
+    def __init__(self, key, data):
         self.data = data
         self.key = key
-        self.measure_unit = measure_unit
         self.value = 0.0
-        if calibrate_factor is not None:
-            self.calibrate_factor = calibrate_factor
-        else:
-            self.calibrate_factor = 1
 
     def get_value(self):
-        return round(self.value * self.calibrate_factor, 2)
+        return round(self.value, 2)
 
 
 def download_url_data():
@@ -43,22 +37,10 @@ def parse_sensor_data():
     temp_array = []
     if len(object_array) > 0:
         for item in object_array:
-            temp_array.append(Sensor(name=item['sensorName'],
-                                     key=item['sensorMapKey'],
-                                     data=item['sensorData'],
-                                     measure_unit=item['sensorUnit'],
-                                     calibrate_factor=item['sensorCalib']))
+            temp_array.append(Sensor(key=item['sensorMapKey'],
+                                     data=item['sensorData']))
 
     return temp_array
-
-
-def update_data_from_url(data):
-    data_json = download_url_data()
-    object_array = data_json[find_index_from_key_value(data_json, "CPUSerial", constant.CPU_SERIAL)]['SensorData']
-    if 0 < len(data) == len(object_array) and len(object_array) > 0:
-        for item in data:
-            item.calibrate_factor = object_array[find_index_from_key_value(object_array, "sensorMapKey", item.key)]['sensorCalib']
-    return data
 
 
 def read_sensor_data(ser, data):
@@ -71,7 +53,7 @@ def read_sensor_data(ser, data):
 
 def publish_data_to_mqtt_server(device_client, data):
     print("Publish data to MQTT Server")
-    device_client.publish(str(constant.MQTT_TOPIC + constant.IOTHUB_DEVICE_DPS_DEVICE_ID), json.dumps(data), 0, True)
+    device_client.publish(str(constant.MQTT_TOPIC + constant.CPU_SERIAL), json.dumps(data), 0, True)
 
 
 def write_serial_data(ser, data):
@@ -96,246 +78,3 @@ def read_serial_data(ser):
         else:
             return 0
     return 0
-    
-def serial_read_data(ser, length):
-    
-    bytesToRead = ser.inWaiting()
-    if bytesToRead > 0:
-        out = ser.read(bytesToRead)
-        data_array = [b for b in out]
-        if len(data_array) >= 7:
-            array_size = len(data_array)
-            value = data_array[array_size - 4] * 256 + data_array[array_size - 3]
-            return value
-        else:
-            return 0
-    return 0
-###################################################################
-def calCRC16(data):
-    crc_table=[0x0000,0xC0C1,0xC181,0x0140,0xC301,0x03C0,0x0280,0xC241,0xC601,0x06C0,0x0780,0xC741,0x0500,0xC5C1,0xC481,0x0440,0xCC01,0x0CC0,0x0D80,0xCD41,0x0F00,0xCFC1,0xCE81,0x0E40,0x0A00,0xCAC1,0xCB81,0x0B40,0xC901,0x09C0,0x0880,0xC841,0xD801,0x18C0,0x1980,0xD941,0x1B00,0xDBC1,0xDA81,0x1A40,0x1E00,0xDEC1,0xDF81,0x1F40,0xDD01,0x1DC0,0x1C80,0xDC41,0x1400,0xD4C1,0xD581,0x1540,0xD701,0x17C0,0x1680,0xD641,0xD201,0x12C0,0x1380,0xD341,0x1100,0xD1C1,0xD081,0x1040,0xF001,0x30C0,0x3180,0xF141,0x3300,0xF3C1,0xF281,0x3240,0x3600,0xF6C1,0xF781,0x3740,0xF501,0x35C0,0x3480,0xF441,0x3C00,0xFCC1,0xFD81,0x3D40,0xFF01,0x3FC0,0x3E80,0xFE41,0xFA01,0x3AC0,0x3B80,0xFB41,0x3900,0xF9C1,0xF881,0x3840,0x2800,0xE8C1,0xE981,0x2940,0xEB01,0x2BC0,0x2A80,0xEA41,0xEE01,0x2EC0,0x2F80,0xEF41,0x2D00,0xEDC1,0xEC81,0x2C40,0xE401,0x24C0,0x2580,0xE541,0x2700,0xE7C1,0xE681,0x2640,0x2200,0xE2C1,0xE381,0x2340,0xE101,0x21C0,0x2080,0xE041,0xA001,0x60C0,0x6180,0xA141,0x6300,0xA3C1,0xA281,0x6240,0x6600,0xA6C1,0xA781,0x6740,0xA501,0x65C0,0x6480,0xA441,0x6C00,0xACC1,0xAD81,0x6D40,0xAF01,0x6FC0,0x6E80,0xAE41,0xAA01,0x6AC0,0x6B80,0xAB41,0x6900,0xA9C1,0xA881,0x6840,0x7800,0xB8C1,0xB981,0x7940,0xBB01,0x7BC0,0x7A80,0xBA41,0xBE01,0x7EC0,0x7F80,0xBF41,0x7D00,0xBDC1,0xBC81,0x7C40,0xB401,0x74C0,0x7580,0xB541,0x7700,0xB7C1,0xB681,0x7640,0x7200,0xB2C1,0xB381,0x7340,0xB101,0x71C0,0x7080,0xB041,0x5000,0x90C1,0x9181,0x5140,0x9301,0x53C0,0x5280,0x9241,0x9601,0x56C0,0x5780,0x9741,0x5500,0x95C1,0x9481,0x5440,0x9C01,0x5CC0,0x5D80,0x9D41,0x5F00,0x9FC1,0x9E81,0x5E40,0x5A00,0x9AC1,0x9B81,0x5B40,0x9901,0x59C0,0x5880,0x9841,0x8801,0x48C0,0x4980,0x8941,0x4B00,0x8BC1,0x8A81,0x4A40,0x4E00,0x8EC1,0x8F81,0x4F40,0x8D01,0x4DC0,0x4C80,0x8C41,0x4400,0x84C1,0x8581,0x4540,0x8701,0x47C0,0x4680,0x8641,0x8201,0x42C0,0x4380,0x8341,0x4100,0x81C1,0x8081,0x4040]
-
-    crc_hi=0xFF
-    crc_lo=0xFF
-
-    for w in data:
-        index = crc_lo ^ w
-        crc_val = crc_table[index]
-        crc_temp = crc_val >> 8
-        crc_val_low = crc_val-(crc_temp << 8)
-        crc_lo = crc_val_low ^ crc_hi
-        crc_hi = crc_temp
-
-    crc = []
-    crc.append (crc_lo)
-    crc.append (crc_hi)
-    return crc
-
-
-def addCRC16(data):
-    crc = calCRC16(data)
-    data.append(crc[0])
-    data.append(crc[1])
-    return data
-
-
-def checkCRC16(data):
-    crc1 = []
-    crc1.append(data[len(data) - 2])
-    crc1.append(data[len(data) - 1])
-    data = data[:-2]
-
-    crc2 = calCRC16(data)
-    if (crc1[0] == crc2[0]) and (crc1[1] == crc2[1]):
-        return True
-    return False
-
-
-
-
-
-def read_data_sensor(ser, data, key, temperature):
-    if ser.isOpen():
-        ser.write(serial.to_bytes(data))
-        time.sleep(0.5)
-        result = serial_read_data(serial, 100)
-        return modify_sensor(key, result[len(result) - 3] * 256 + result[len(result) - 4], temperature)
-    else:
-        return 0
-
-
-
-def setPump(ser, state):
-    if state == True:
-        relay_on = [0x0F, 0x06, 0x00, 0x00, 0x00, 0xFF]
-        ser.write(addCRC16(relay_on))
-        result = serial_read_data(ser, 100)
-    else:
-        relay_off = [0x0F, 0x06, 0x00, 0x00, 0x00, 0x00]
-        ser.write(addCRC16(relay_off))
-        result = serial_read_data(ser, 100)
-
-
-def setFlush(ser, state):
-    if state == True:
-        relay_on = [0x00, 0x06, 0x00, 0x00, 0x00, 0xFF]
-        ser.write(addCRC16(relay_on))
-        result = serial_read_data(ser, 100)
-    else:
-        relay_off = [0x00, 0x06, 0x00, 0x00, 0x00, 0x00]
-        ser.write(addCRC16(relay_off))
-        result = serial_read_data(ser, 100)
-
-
-def readPumpLevel(ser):
-    adc2 = [0x02, 0x04, 0x00, 0x00, 0x00, 0x01]
-    ser.write(addCRC16(adc2))
-    result = serial_read_data(ser, 100)
-
-    return result
-
-
-def Flush_Water(ser):
-    counter_timer = 90
-    setFlush(ser, True)
-    time.sleep(0.5)
-    setFlush(ser, True)
-    while True:
-        counter_timer = counter_timer - 1
-        if counter_timer % 10 == 0:
-            print(counter_timer)
-        if counter_timer == 0:
-            break
-        
-        time.sleep(1)
-
-    setFlush(ser, False)
-    time.sleep(0.5)
-    setFlush(ser, False)
-
-
-def Pump_Water(ser, counter_timer):
-    #counter_timer = 105
-    
-    setPump(ser, True)
-    time.sleep(0.5)
-    setPump(ser, True)
-
-    while True:
-        counter_timer = counter_timer - 1
-        if(counter_timer == 0):
-            break
-        if(readPumpLevel(ser) > 1000):
-            break
-        time.sleep(0.5)
-
-    setPump(ser, False)
-    time.sleep(0.5)
-    setPump(ser, False)
-
-
-def read_temp_raw(file_path):
-    f = open(file_path, 'r')
-    lines = f.readlines()
-    f.close()
-    return lines
-
-
-def river_water_level(ser):
-    data = [0x01, 0x03, 0x00, 0x02, 0x00, 0x02, 0x65, 0xCB]
-    serial.write(data)
-    result = serial_read_data(serial, 100)
-    
-    return result
-
-def read_temp(file_path):
-    lines = read_temp_raw(file_path)
-    
-    while lines[0].strip()[-3:] != 'YES':
-        time.sleep(0.2)
-        lines = read_temp_raw(file_path)
-
-    equals_pos = lines[1].find('t=')
-    if equals_pos != -1:
-        temp_string = lines[1][equals_pos + 2:]
-        temp_c = float(temp_string) / 1000.0
-        return round(temp_c, 2)
-
-
-def modify_sensor(key, value, temperature):
-    if key == "turbidity":
-        return modify_turbidity(value, temperature)
-    elif key == "tds":
-        return modify_tds(value, temperature)
-    elif key == "ph":
-        return modify_ph(value, temperature)
-    elif key == "do":
-        return modify_do(value, temperature)
-    elif key == "ec":
-        return modify_ec(value, temperature)
-    else:
-        return value
-
-
-def modify_tds(value, temperature):
-    averageVoltage = value * 5.0 / 4096.0
-    
-    compensationCoefficient=1.0+0.02*(temperature-25.0)
-    compensationVolatge=averageVoltage/compensationCoefficient
-    
-    tdsValue=(133.42*compensationVolatge*compensationVolatge*compensationVolatge - 255.86*compensationVolatge*compensationVolatge + 857.39*compensationVolatge)*0.5
-    
-    return tdsValue
-
-def modify_turbidity(value, temperature):
-    x = value * 5.0 / 4096.0
-    print("Turbidity (V)", x)
-    #y = 3000 - (x * 781.25)
-    y = -1120.4 * x * x + 5742.3*x - 4352.9
-    if y < 0:
-        y = 3000
-    print("Turbidity (NTU)", y)
-    return y
-
-def modify_ph(value, temperature):
-    neutralVoltage = 900 #1500
-    acidVoltage = 1350 #2032.44
-    
-    voltage = value * 5 / 4096.0
-    voltage = voltage * 1000
-    print("PH(mV)", voltage)
-    
-    
-    slope = (7.0-4.0)/((neutralVoltage-1500.0)/3.0 - (acidVoltage-1500.0)/3.0)
-    intercept = 7.0 - slope*(neutralVoltage-1500.0)/3.0
-    
-    phValue = slope*(voltage-1500.0)/3.0+intercept
-    print("PH: ", phValue)
-    return phValue
-
-def modify_do(value, temperature):
-    CAL1_T = 25
-    CAL1_V = 1600
-    DO_Table= [
-    14460, 14220, 13820, 13440, 13090, 12740, 12420, 12110, 11810, 11530,
-    11260, 11010, 10770, 10530, 10300, 10080, 9860, 9660, 9460, 9270,
-    9080, 8900, 8730, 8570, 8410, 8250, 8110, 7960, 7820, 7690,
-    7560, 7430, 7300, 7180, 7070, 6950, 6840, 6730, 6630, 6530, 6410]
-    Temperaturet = int(temperature)
-    voltage = value * 5000 / 4096.0 #mV
-    print("DO Voltage", voltage)
-    V_saturation = CAL1_V + 35 * (Temperaturet - CAL1_T)
-    
-    value = voltage * DO_Table[Temperaturet] / V_saturation
-    
-    return value/1000
- 
-def modify_ec(value, temperature):
-    #RES2 = 820.0
-    #ECREF = 200.0
-    GAIN = 8.5
-    voltage = value * 5 / 4096.0
-    rawEC = 1000*voltage * GAIN
-    value = rawEC / (1.0+0.0185*(temperature-25.0))
-    
-    return value
-    
--- main.py
+++ main.py
@@ -1,425 +1,78 @@
 #!/usr/bin/python3
 
-# -------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See License.txt in the project root for
-# license information.
-# --------------------------------------------------------------------------
-import os
-import asyncio
-import random
-import logging
 import json
 import time
-import glob
 from serial import Serial
 import paho.mqtt.client as mqtt
 import constant as const_var
 import function
 
-from azure.iot.device.aio import IoTHubDeviceClient
-from azure.iot.device.aio import ProvisioningDeviceClient
-from azure.iot.device import constant, Message, MethodResponse
 from datetime import date, timedelta, datetime
 
-logging.basicConfig(level=logging.ERROR)
-
-# The device "Thermostat" that is getting implemented using the above interfaces.
-# This id can change according to the company the user is from
-# and the name user wants to call this Plug and Play device
-model_id = "dtmi:com:example:Thermostat;1"
 
 #####################################################
 # GLOBAL THERMOSTAT VARIABLES
 sensor_array = []
-water_temperature_value = 0.0
 
 data_payload = {
-    "project_id": const_var.PROJECT_ID,
-    "project_name": const_var.PROJECT_NAME,
-    "station_id": str(const_var.STATION_ID + const_var.IOTHUB_DEVICE_DPS_DEVICE_ID),
-    "station_name": str(const_var.STATION_NAME + const_var.IOTHUB_DEVICE_DPS_DEVICE_ID),
-    "longitude": 106.660172,
-    "latitude": 10.762622,
-    "volt_battery": 12.5,
-    "volt_solar": 5.3
+  "project_id": const_var.PROJECT_ID,
+  "station_id": str(const_var.STATION_ID + const_var.CPU_SERIAL),
+  "longitude": 106.660172,
+  "latitude": 10.762622,
+  "volt_battery": 12.5,
+  "volt_solar": 5.3
 }
 
-BASE_DIR = '/sys/bus/w1/devices/'
-DEVICE_DIR = glob.glob(BASE_DIR + '28*')[0]
-DEVICE_FILE = DEVICE_DIR + '/w1_slave'
-
-#####################################################
-# COMMAND HANDLERS : User will define these handlers
-# depending on what commands the DTMI defines
-
-
-async def reboot_handler(values):
-    if values and type(values) == int:
-        print("Rebooting after delay of {delay} secs".format(delay=values))
-        asyncio.sleep(values)
-    print("Done rebooting")
-
-
-async def max_min_handler(values):
-    if values:
-        print(
-            "Will return the max, min and average temperature from the specified time {since} to the current time".format(
-                since=values
-            )
-        )
-    print("Done generating")
-
-
-# END COMMAND HANDLERS
 #####################################################
 
-#####################################################
-# CREATE RESPONSES TO COMMANDS
+def send_telemetry(mqtt_client, serial_communication):
+  print("Start sending telemetry")
+  global sensor_array
+  count_timer = 0
 
+  while True:
+    time.sleep(1)
+    count_timer += 1
+    
+    if count_timer % const_var.TIME_CYCLE == 0:
+      if len(sensor_array) > 0:
+          for index in range(0, len(sensor_array)):
+              if const_var.STATION_TYPE == "AIR_SOIL":
+                  sensor_array[index].value = function.read_sensor_data(serial_communication, sensor_array[index].data)
+      else:
+        print("No sensor data")
 
-def create_max_min_report_response(values):
-    """
-    An example function that can create a response to the "getMaxMinReport" command request the way the user wants it.
-    Most of the times response is created by a helper function which follows a generic pattern.
-    This should be only used when the user wants to give a detailed response back to the Hub.
-    :param values: The values that were received as part of the request.
-    """
-    response_dict = {
-        # "startTime": (datetime.now() - timedelta(0, moving_window_size * 8)).isoformat(),
-        "endTime": datetime.now().isoformat(),
-    }
-    # serialize response dictionary into a JSON formatted str
-    response_payload = json.dumps(response_dict, default=lambda o: o.__dict__, sort_keys=True)
-    print(response_payload)
-    return response_payload
+      function.publish_data_to_mqtt_server(mqtt_client, update_data_payload())
+      time.sleep(1)
 
-
-def create_reboot_response(values):
-    response = {"result": True, "data": "reboot succeeded"}
-    return response
-
-
-# END CREATE RESPONSES TO COMMANDS
-#####################################################
-
-#####################################################
-# TELEMETRY TASKS
-
-
-async def send_telemetry_from_thermostat(device_client, telemetry_msg):
-    msg = Message(json.dumps(telemetry_msg))
-    msg.content_encoding = "utf-8"
-    msg.content_type = "application/json"
-    print("Sent message")
-    await device_client.send_message(msg)
-
-
-# END TELEMETRY TASKS
-#####################################################
-
-#####################################################
-# CREATE COMMAND AND PROPERTY LISTENERS
-
-
-async def execute_command_listener(
-    device_client, method_name, user_command_handler, create_user_response_handler
-):
-    while True:
-        if method_name:
-            command_name = method_name
-        else:
-            command_name = None
-
-        command_request = await device_client.receive_method_request(command_name)
-        print("Command request received with payload")
-        print(command_request.payload)
-
-        values = {}
-        if not command_request.payload:
-            print("Payload was empty.")
-        else:
-            values = command_request.payload
-
-        await user_command_handler(values)
-
-        response_status = 200
-        response_payload = create_user_response_handler(values)
-
-        command_response = MethodResponse.create_from_method_request(
-            command_request, response_status, response_payload
-        )
-
-        try:
-            await device_client.send_method_response(command_response)
-        except Exception:
-            print("responding to the {command} command failed".format(command=method_name))
-
-
-async def execute_property_listener(device_client):
-    ignore_keys = ["__t", "$version"]
-    while True:
-        patch = await device_client.receive_twin_desired_properties_patch()  # blocking call
-
-        print("the data in the desired properties patch was: {}".format(patch))
-
-        version = patch["$version"]
-        prop_dict = {}
-
-        for prop_name, prop_value in patch.items():
-            if prop_name in ignore_keys:
-                continue
-            else:
-                prop_dict[prop_name] = {
-                    "ac": 200,
-                    "ad": "Successfully executed patch",
-                    "av": version,
-                    "value": prop_value,
-                }
-
-        await device_client.patch_twin_reported_properties(prop_dict)
-
-
-# END COMMAND AND PROPERTY LISTENERS
-#####################################################
-
-#####################################################
-# An # END KEYBOARD INPUT LISTENER to quit application
-
-
-def stdin_listener():
-    """
-    Listener for quitting the sample
-    """
-    while True:
-        selection = "."
-        if selection == "Q" or selection == "q":
-            print("Quitting...")
-            break
-
-
-# END KEYBOARD INPUT LISTENER
-#####################################################
-
-
-#####################################################
-# PROVISION DEVICE
-async def provision_device(provisioning_host, id_scope, registration_id, symmetric_key, model_id):
-    provisioning_device_client = ProvisioningDeviceClient.create_from_symmetric_key(
-        provisioning_host=provisioning_host,
-        registration_id=registration_id,
-        id_scope=id_scope,
-        symmetric_key=symmetric_key,
-    )
-    provisioning_device_client.provisioning_payload = {"modelId": model_id}
-    return await provisioning_device_client.register()
-
-
-#####################################################
-# MAIN STARTS
-async def main():
-    switch = os.getenv("IOTHUB_DEVICE_SECURITY_TYPE")
-    if switch == "DPS":
-        provisioning_host = (
-            os.getenv("IOTHUB_DEVICE_DPS_ENDPOINT")
-            if os.getenv("IOTHUB_DEVICE_DPS_ENDPOINT")
-            else "global.azure-devices-provisioning.net"
-        )
-        id_scope = os.getenv("IOTHUB_DEVICE_DPS_ID_SCOPE")
-        registration_id = os.getenv("IOTHUB_DEVICE_DPS_DEVICE_ID")
-        symmetric_key = os.getenv("IOTHUB_DEVICE_DPS_DEVICE_KEY")
-
-        registration_result = await provision_device(
-            provisioning_host, id_scope, registration_id, symmetric_key, model_id
-        )
-
-        if registration_result.status == "assigned":
-            print("Device was assigned")
-            print(registration_result.registration_state.assigned_hub)
-            print(registration_result.registration_state.device_id)
-
-            device_client = IoTHubDeviceClient.create_from_symmetric_key(
-                symmetric_key=symmetric_key,
-                hostname=registration_result.registration_state.assigned_hub,
-                device_id=registration_result.registration_state.device_id,
-                product_info=model_id,
-            )
-        else:
-            raise RuntimeError(
-                "Could not provision device. Aborting Plug and Play device connection."
-            )
-
-    elif switch == "connectionString":
-        conn_str = os.getenv("IOTHUB_DEVICE_CONNECTION_STRING")
-        print("Connecting using Connection String " + conn_str)
-        device_client = IoTHubDeviceClient.create_from_connection_string(
-            conn_str, product_info=model_id
-        )
-    else:
-        raise RuntimeError(
-            "At least one choice needs to be made for complete functioning of this sample."
-        )
-
-    # Connect the client.
-    await device_client.connect()
-
-    ################################################
-    # Set and read desired property (target temperature)
-
-    max_temp = 10.96  # Initial Max Temp otherwise will not pass certification
-    await device_client.patch_twin_reported_properties({"maxTempSinceLastReboot": max_temp})
-
-    ################################################
-    # Register callback and Handle command (reboot)
-    print("Listening for command requests and property updates")
-
-    listeners = asyncio.gather(
-        execute_command_listener(
-            device_client,
-            method_name="reboot",
-            user_command_handler=reboot_handler,
-            create_user_response_handler=create_reboot_response,
-        ),
-        execute_command_listener(
-            device_client,
-            method_name="getMaxMinReport",
-            user_command_handler=max_min_handler,
-            create_user_response_handler=create_max_min_report_response,
-        ),
-        execute_property_listener(device_client),
-    )
-
-    ################################################
-    # Send telemetry (current temperature)
-
-    async def send_telemetry():
-        print("Sending telemetry for temperature")
-        global mqttClient
-        global serialCommunication
-        global sensor_array
-        global water_temperature_value
-        global DEVICE_FILE
-
-        count_timer = 0
-        while True:
-            time.sleep(1)
-            count_timer += 1
-            
-            if count_timer % const_var.TIME_CYCLE == 0:
-                if const_var.STATION_TYPE == "WATER":
-                    function.Pump_Water(serialCommunication, 105)
-                    time.sleep(30)
-                    function.Pump_Water(serialCommunication, 35)
-                    time.sleep(5)
-                    water_temperature_value = function.read_temp(DEVICE_FILE)
-                    
-                if len(sensor_array) > 0:
-                    for index in range(0, len(sensor_array)):
-                        if const_var.STATION_TYPE == "AIR_SOIL":
-                            sensor_array[index].value = function.read_sensor_data(serialCommunication,
-                                                                                  sensor_array[index].data)
-                        elif const_var.STATION_TYPE == "WATER":
-                            sensor_array[index].value = function.read_data_sensor(ser=serialCommunication,
-                                                                                  data=sensor_array[index].data,
-                                                                                  key=sensor_array[index].key,
-                                                                                  temperature=water_temperature_value)
-                else:
-                    print("No sensor data")
-
-                if const_var.STATION_TYPE == "WATER":
-                    function.Flush_Water(serialCommunication)
-                    time.sleep(3)
-
-                function.publish_data_to_mqtt_server(mqttClient, update_data_payload())
-                time.sleep(5)
-                await send_telemetry_from_thermostat(device_client, update_data_sensor())
-                await asyncio.sleep(8)
-
-            if count_timer > const_var.REQUEST_CYCLE:
-                sensor_array = function.update_data_from_url(sensor_array)
-                count_timer = 0
-
-    loop = asyncio.get_event_loop()
-    send_telemetry_task = loop.create_task(send_telemetry())
-
-    # Run the stdin listener in the event loop
-    #loop = asyncio.get_running_loop()
-    user_finished = loop.run_in_executor(None, stdin_listener)
-    # # Wait for user to indicate they are done listening for method calls
-    await user_finished
-
-    if not listeners.done():
-        listeners.set_result("DONE")
-
-    listeners.cancel()
-
-    send_telemetry_task.cancel()
-
-    # Finally, shut down the client
-    await device_client.shutdown()
-
-
-def update_data_sensor():
-    global sensor_array
-    global water_temperature_value
-    data_json = {}
-    if len(sensor_array) > 0:
-        for item in sensor_array:
-            data_json[item.key] = item.get_value()
-    if const_var.STATION_TYPE == "WATER":
-        data_json[const_var.WATER_TEMPERATURE_KEY] = water_temperature_value
-
-    return json.loads(data_json)
-
+      count_timer = 0
+      
 
 def update_data_payload():
-    global data_payload
-    global sensor_array
-    global water_temperature_value
-    data_json_array = []
-    if len(sensor_array) > 0:
-        for item in sensor_array:
-            json_object = {'sensor_name': item.name,
-                           'sensor_key': item.key,
-                           'sensor_unit': item.measure_unit,
-                           'sensor_value': item.get_value()}
-            data_json_array.append(json_object)
+  global data_payload
+  global sensor_array
+  data_json_array = []
+  if len(sensor_array) > 0:
+    for item in sensor_array:
+      json_object = {'sensor_key': item.key, 'sensor_value': item.get_value()}
+      data_json_array.append(json_object)
 
-    if const_var.STATION_TYPE == "WATER":
-        json_object = {'sensor_name': const_var.WATER_TEMPERATURE_NAME,
-                       'sensor_key': const_var.WATER_TEMPERATURE_KEY,
-                       'sensor_unit': const_var.WATER_TEMPERATURE_UNIT,
-                       'sensor_value': water_temperature_value}
-        data_json_array.append(json_object)
+  data_payload["data_sensor"] = data_json_array
 
-    data_payload["data_sensor"] = data_json_array
-
-    return data_payload
+  return data_payload
 
 
 #####################################################
 # EXECUTE MAIN
 
 if __name__ == "__main__":
-    os.environ['IOTHUB_DEVICE_SECURITY_TYPE'] = const_var.IOTHUB_DEVICE_SECURITY_TYPE
-    os.environ['IOTHUB_DEVICE_DPS_ID_SCOPE'] = const_var.IOTHUB_DEVICE_DPS_ID_SCOPE
-    os.environ['IOTHUB_DEVICE_DPS_DEVICE_ID'] = const_var.IOTHUB_DEVICE_DPS_DEVICE_ID
-    os.environ['IOTHUB_DEVICE_DPS_DEVICE_KEY'] = const_var.IOTHUB_DEVICE_DPS_DEVICE_KEY
-    os.environ['IOTHUB_DEVICE_DPS_ENDPOINT'] = const_var.IOTHUB_DEVICE_DPS_ENDPOINT
+  sensor_array = function.parse_sensor_data()
 
-    os.system('modprobe w1-gpio')
-    os.system('modprobe w1-therm')
+  mqttClient = mqtt.Client()
+  mqttClient.username_pw_set(const_var.MQTT_USERNAME, const_var.MQTT_PASSWORD)
+  mqttClient.connect(const_var.MQTT_SERVER, int(const_var.MQTT_PORT), 60)
+  mqttClient.loop_start()
 
-    sensor_array = function.parse_sensor_data()
-    sensor_array = function.update_data_from_url(sensor_array)
+  serialCommunication = Serial(const_var.SERIAL_PORT, const_var.SERIAL_BAUDRATE)
 
-    mqttClient = mqtt.Client()
-    mqttClient.username_pw_set(const_var.MQTT_USERNAME, const_var.MQTT_PASSWORD)
-    mqttClient.connect(const_var.MQTT_SERVER, int(const_var.MQTT_PORT), 60)
-    mqttClient.loop_start()
-
-    serialCommunication = Serial(const_var.SERIAL_PORT, const_var.SERIAL_BAUDRATE)
-
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
-    loop.close()
+  send_telemetry()
